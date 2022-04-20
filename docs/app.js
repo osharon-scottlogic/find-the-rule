be32e73c71c3f152d4a44ffe7b94f40163f47f89
Oded Sharon <osharon@scottlogic.com>
2022-04-20 18:45:58 +0100

    encapsulating the entire code

--- docs/app.js
+++ docs/app.js
@@ -34,171 +34,173 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     return to.concat(ar || Array.prototype.slice.call(from));
 };
 import { h, render, Component } from 'https://unpkg.com/preact?module';
-var HypothesisTest = /** @class */ (function () {
-    function HypothesisTest(assumption) {
-        this.assumption = assumption;
+(function () {
+    var HypothesisTest = /** @class */ (function () {
+        function HypothesisTest(assumption) {
+            this.assumption = assumption;
+        }
+        HypothesisTest.prototype.test = function (a, b, c) {
+            return eval(this.assumption);
+        };
+        return HypothesisTest;
+    }());
+    var rules = [
+        [function (a, b, c) { return (a < b && b < c); }],
+        [
+            function (a, b, c) { return (a > b && b > c); },
+            function (a, b, c) { return (a + b < c); },
+            function (a, b, c) { return (a + b > c); },
+            function (a, b, c) { return (a + c < b); },
+            function (a, b, c) { return (a + c > b); },
+            function (a, b, c) { return (b + c < a); },
+            function (a, b, c) { return (b + c > a); },
+            function (a, b, c) { return (a + b === a); },
+            function (a, b, c) { return (a + c === b); },
+            function (a, b, c) { return (a + b === c); }
+        ],
+        [
+            function (a, b, c) { return (a <= b && b <= c); },
+            function (a, b, c) { return (a <= b && b < c); },
+            function (a, b, c) { return (a < b && b <= c); },
+            function (a, b, c) { return (a >= b && b >= c); },
+            function (a, b, c) { return (a > b && b >= c); },
+            function (a, b, c) { return (a >= b && b > c); },
+            function (a, b, c) { return (a + b <= c); },
+            function (a, b, c) { return (a + b >= c); },
+            function (a, b, c) { return (a + c <= b); },
+            function (a, b, c) { return (a + c >= b); },
+            function (a, b, c) { return (b + c <= a); },
+            function (a, b, c) { return (b + c >= a); }
+        ]
+    ];
+    var rule = rules[0][0];
+    function randomPick(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
     }
-    HypothesisTest.prototype.test = function (a, b, c) {
-        return eval(this.assumption);
-    };
-    return HypothesisTest;
-}());
-var rules = [
-    [function (a, b, c) { return (a < b && b < c); }],
-    [
-        function (a, b, c) { return (a > b && b > c); },
-        function (a, b, c) { return (a + b < c); },
-        function (a, b, c) { return (a + b > c); },
-        function (a, b, c) { return (a + c < b); },
-        function (a, b, c) { return (a + c > b); },
-        function (a, b, c) { return (b + c < a); },
-        function (a, b, c) { return (b + c > a); },
-        function (a, b, c) { return (a + b === a); },
-        function (a, b, c) { return (a + c === b); },
-        function (a, b, c) { return (a + b === c); }
-    ],
-    [
-        function (a, b, c) { return (a <= b && b <= c); },
-        function (a, b, c) { return (a <= b && b < c); },
-        function (a, b, c) { return (a < b && b <= c); },
-        function (a, b, c) { return (a >= b && b >= c); },
-        function (a, b, c) { return (a > b && b >= c); },
-        function (a, b, c) { return (a >= b && b > c); },
-        function (a, b, c) { return (a + b <= c); },
-        function (a, b, c) { return (a + b >= c); },
-        function (a, b, c) { return (a + c <= b); },
-        function (a, b, c) { return (a + c >= b); },
-        function (a, b, c) { return (b + c <= a); },
-        function (a, b, c) { return (b + c >= a); }
-    ]
-];
-var rule = rules[0][0];
-function randomPick(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
-function getState(level) {
-    if (level < rules.length - 1) {
-        level++;
+    function getState(level) {
+        if (level < rules.length - 1) {
+            level++;
+        }
+        rule = randomPick(rules[level]);
+        return {
+            tests: [{ a: 1, b: 2, c: 3, actual: rule(1, 2, 3) }],
+            assumption: '',
+            result: undefined,
+            val: { a: '', b: '', c: '' },
+            level: level
+        };
     }
-    rule = randomPick(rules[level]);
-    return {
-        tests: [{ a: 1, b: 2, c: 3, actual: rule(1, 2, 3) }],
-        assumption: '',
-        result: undefined,
-        val: { a: '', b: '', c: '' },
-        level: level,
-    };
-}
-var App = /** @class */ (function (_super) {
-    __extends(App, _super);
-    function App() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.state = getState(0);
-        //@ts-ignore
-        _this.addTest = function (newTest) { return _this.setState(function (state) { return (__assign(__assign({}, state), { tests: __spreadArray(__spreadArray([], state.tests, true), [newTest], false) })); }); };
-        _this.setAssumption = function (assumption) { return _this.setState(function (state) { return (__assign(__assign({}, state), { assumption: assumption })); }); };
-        _this.setResult = function (result) { return _this.setState(function (state) { return (__assign(__assign({}, state), { result: result })); }); };
-        _this.restart = function () { return _this.setState(getState(_this.state.level)); };
-        _this.test = function () {
-            var a = +_this.state.val.a;
-            var b = +_this.state.val.b;
-            var c = +_this.state.val.c;
-            _this.addTest({ a: a, b: b, c: c, actual: rule(a, b, c) });
-        };
-        _this.updateVal = function (key, evt) {
-            var val = __assign({}, _this.state.val);
-            val[key] = +evt.target.value;
-            _this.setState(__assign(__assign({}, _this.state), { val: val }));
-        };
-        _this.updateAssumption = function (evt) { return _this.setAssumption(evt.target.value); };
-        _this.procFormAndCancelSubmit = function (method, evt) {
-            method(evt);
-            evt.preventDefault();
-            return false;
-        };
-        _this.makeAssumption = function () {
-            var assumption = _this.state.assumption;
-            if (!assumption || assumption.length <= 5) {
-                return;
-            }
-            console.log(assumption);
-            if (assumption.indexOf('=>') > -1 && !confirm("'=>' in JS doesn't mean equal-or-great. Do you with to process?")) {
-                return;
-            }
-            var hypothesis = new HypothesisTest(assumption);
-            for (var a = 0; a < 10; a++) {
-                for (var b = 0; b < 10; b++) {
-                    for (var c = 0; c < 10; c++) {
-                        try {
-                            if (hypothesis.test(a, b, c) !== rule(a, b, c)) {
+    var App = /** @class */ (function (_super) {
+        __extends(App, _super);
+        function App() {
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this.state = getState(0);
+            //@ts-ignore
+            _this.addTest = function (newTest) { return _this.setState(function (state) { return (__assign(__assign({}, state), { tests: __spreadArray(__spreadArray([], state.tests, true), [newTest], false) })); }); };
+            _this.setAssumption = function (assumption) { return _this.setState(function (state) { return (__assign(__assign({}, state), { assumption: assumption })); }); };
+            _this.setResult = function (result) { return _this.setState(function (state) { return (__assign(__assign({}, state), { result: result })); }); };
+            _this.restart = function () { return _this.setState(getState(_this.state.level)); };
+            _this.test = function () {
+                var a = +_this.state.val.a;
+                var b = +_this.state.val.b;
+                var c = +_this.state.val.c;
+                _this.addTest({ a: a, b: b, c: c, actual: rule(a, b, c) });
+            };
+            _this.updateVal = function (key, evt) {
+                var val = __assign({}, _this.state.val);
+                val[key] = +evt.target.value;
+                _this.setState(__assign(__assign({}, _this.state), { val: val }));
+            };
+            _this.updateAssumption = function (evt) { return _this.setAssumption(evt.target.value); };
+            _this.procFormAndCancelSubmit = function (method, evt) {
+                method(evt);
+                evt.preventDefault();
+                return false;
+            };
+            _this.makeAssumption = function () {
+                var assumption = _this.state.assumption;
+                if (!assumption || assumption.length <= 5) {
+                    return;
+                }
+                console.log(assumption);
+                if (assumption.indexOf('=>') > -1 && !confirm("'=>' in JS doesn't mean equal-or-great. Do you with to process?")) {
+                    return;
+                }
+                var hypothesis = new HypothesisTest(assumption);
+                for (var a = 0; a < 10; a++) {
+                    for (var b = 0; b < 10; b++) {
+                        for (var c = 0; c < 10; c++) {
+                            try {
+                                if (hypothesis.test(a, b, c) !== rule(a, b, c)) {
+                                    return _this.setResult(false);
+                                }
+                            }
+                            catch (err) {
+                                console.error(err);
                                 return _this.setResult(false);
                             }
                         }
-                        catch (err) {
-                            console.error(err);
-                            return _this.setResult(false);
-                        }
                     }
                 }
-            }
-            _this.setResult(true);
-        };
-        return _this;
-    }
-    App.prototype.render = function (props, state) {
-        return (h("main", null,
-            h("section", { class: "experiment" },
-                h("h2", null, "1. Experiment"),
-                h("form", { onSubmit: this.procFormAndCancelSubmit.bind(null, this.test), className: "testForm" },
-                    h("input", { type: "number", min: 0, max: 99, value: state.val.a, onInput: this.updateVal.bind(null, 'a') }),
-                    h("input", { type: "number", min: 0, max: 99, value: state.val.b, onInput: this.updateVal.bind(null, 'b') }),
-                    h("input", { type: "number", min: 0, max: 99, value: state.val.c, onInput: this.updateVal.bind(null, 'c') }),
-                    h("button", { type: "submit" }, "Check"))),
-            h("aside", { class: "results" },
-                h("h2", null, "2. Observe The Results"),
-                h("ol", null, state.tests.map(function (_a) {
-                    var a = _a.a, b = _a.b, c = _a.c, actual = _a.actual;
-                    return h("li", null,
-                        h("span", null, a),
-                        h("span", null, b),
-                        h("span", null, c),
-                        h("span", null,
-                            " ",
-                            actual ? '✅' : '❌'));
-                }))),
-            h("section", { class: "hyposis" },
-                h("h2", null, "3. Submit Your Hypothesis"),
-                h("div", { class: "warning" }, "Careful! you can only submit once!"),
-                h("form", { onSubmit: this.procFormAndCancelSubmit.bind(null, this.makeAssumption) },
-                    h("code", null,
-                        h("pre", null,
-                            "(a,b,c) => ",
-                            '{',
-                            " return ",
-                            h("input", { value: state.assumption, onInput: this.updateAssumption, type: "text", minLength: 5 }),
-                            " ",
-                            '}',
-                            ";")),
-                    h("button", { type: "submit" }, "Submit")),
-                (state.result !== undefined) ? (state.result !== true ?
-                    h("dialog", { open: true },
-                        h("h2", null, "Fail!"),
-                        "You suggest ",
+                _this.setResult(true);
+            };
+            return _this;
+        }
+        App.prototype.render = function (props, state) {
+            return (h("main", null,
+                h("section", { class: "experiment" },
+                    h("h2", null, "1. Experiment"),
+                    h("form", { onSubmit: this.procFormAndCancelSubmit.bind(null, this.test), className: "testForm" },
+                        h("input", { type: "number", min: 0, max: 99, value: state.val.a, onInput: this.updateVal.bind(null, 'a') }),
+                        h("input", { type: "number", min: 0, max: 99, value: state.val.b, onInput: this.updateVal.bind(null, 'b') }),
+                        h("input", { type: "number", min: 0, max: 99, value: state.val.c, onInput: this.updateVal.bind(null, 'c') }),
+                        h("button", { type: "submit" }, "Check"))),
+                h("aside", { class: "results" },
+                    h("h2", null, "2. Observe The Results"),
+                    h("ol", null, state.tests.map(function (_a) {
+                        var a = _a.a, b = _a.b, c = _a.c, actual = _a.actual;
+                        return h("li", null,
+                            h("span", null, a),
+                            h("span", null, b),
+                            h("span", null, c),
+                            h("span", null,
+                                " ",
+                                actual ? '✅' : '❌'));
+                    }))),
+                h("section", { class: "hyposis" },
+                    h("h2", null, "3. Submit Your Hypothesis"),
+                    h("div", { class: "warning" }, "Careful! you can only submit once!"),
+                    h("form", { onSubmit: this.procFormAndCancelSubmit.bind(null, this.makeAssumption) },
                         h("code", null,
-                            h("pre", { class: "expected" },
-                                "function (a, b, c) ",
+                            h("pre", null,
+                                "(a,b,c) => ",
                                 '{',
-                                " return (",
-                                state.assumption,
-                                "); ",
-                                '}')),
-                        "But the rule was ",
-                        h("code", null,
-                            h("pre", { class: "actual" }, rule.toString())),
-                        h("button", { onClick: this.restart, class: "restart" }, "\uD83D\uDD04 Try Another")) : h("dialog", { open: true },
-                    "Well done!",
-                    h("button", { onClick: this.restart, class: "restart" }, "\uD83D\uDD04 Try Another"))) : '')));
-    };
-    return App;
-}(Component));
-window.addEventListener('load', function () { return render(h(App, null), document.body); });
+                                " return ",
+                                h("input", { value: state.assumption, onInput: this.updateAssumption, type: "text", minLength: 5 }),
+                                " ",
+                                '}',
+                                ";")),
+                        h("button", { type: "submit" }, "Submit")),
+                    (state.result !== undefined) ? (state.result !== true ?
+                        h("dialog", { open: true },
+                            h("h2", null, "Fail!"),
+                            "You suggest ",
+                            h("code", null,
+                                h("pre", { class: "expected" },
+                                    "function (a, b, c) ",
+                                    '{',
+                                    " return (",
+                                    state.assumption,
+                                    "); ",
+                                    '}')),
+                            "But the rule was ",
+                            h("code", null,
+                                h("pre", { class: "actual" }, rule.toString())),
+                            h("button", { onClick: this.restart, class: "restart" }, "\uD83D\uDD04 Try Another")) : h("dialog", { open: true },
+                        "Well done!",
+                        h("button", { onClick: this.restart, class: "restart" }, "\uD83D\uDD04 Try Another"))) : '')));
+        };
+        return App;
+    }(Component));
+    window.addEventListener('load', function () { return render(h(App, null), document.body); });
+})();
--- src/app.tsx
+++ src/app.tsx
@@ -1,173 +1,175 @@
 import { h, render, Component } from 'https://unpkg.com/preact?module';
 
-type Test = {a:number,b:number,c:number, expected?:boolean, actual: boolean}
-type Values = {a:number|'', b:number|'', c:number|''};
-type State = {tests:Test[], level: number, assumption?:string, result?:boolean, val: Values};
-type Rule = (a:number, b:number, c:number)=>boolean;
+(() =>{
+	type Test = {a:number,b:number,c:number, expected?:boolean, actual: boolean}
+	type Values = {a:number|'', b:number|'', c:number|''};
+	type State = {tests:Test[], level: number, assumption?:string, result?:boolean, val: Values};
+	type Rule = (a:number, b:number, c:number)=>boolean;
 
-class HypothesisTest {
-	assumption:string;
+	class HypothesisTest {
+		assumption:string;
 
-	constructor(assumption:string) {
-		this.assumption = assumption;
-	}
-
-	test(a:number,b:number,c:number) {
-		return eval(this.assumption);
-	}
-}
-
-const rules:Rule[][] = [
-	[(a,b,c)=>(a<b && b<c)],
-	[
-		(a,b,c)=>(a>b && b>c),
-		(a,b,c)=>(a+b < c),
-		(a,b,c)=>(a+b > c),
-		(a,b,c)=>(a+c < b),
-		(a,b,c)=>(a+c > b),
-		(a,b,c)=>(b+c < a),
-		(a,b,c)=>(b+c > a),
-		(a,b,c)=>(a+b === a),
-		(a,b,c)=>(a+c === b),
-		(a,b,c)=>(a+b === c)
-	],
-	[
-		(a,b,c)=>(a<=b && b<=c),
-		(a,b,c)=>(a<=b && b<c),
-		(a,b,c)=>(a<b && b<=c),
-		(a,b,c)=>(a>=b && b>=c),
-		(a,b,c)=>(a>b && b>=c),
-		(a,b,c)=>(a>=b && b>c),
-		(a,b,c)=>(a+b <= c),
-		(a,b,c)=>(a+b >= c),
-		(a,b,c)=>(a+c <= b),
-		(a,b,c)=>(a+c >= b),
-		(a,b,c)=>(b+c <= a),
-		(a,b,c)=>(b+c >= a)
-	]];
-
-let rule:Rule = rules[0][0];
-
-function randomPick(arr:any[]):any {
-	return arr[Math.floor(Math.random()*arr.length)];
-}
-
-function getState(level:number):State {
-	if (level < rules.length - 1) {
-		level++;
-	}
-	
-	rule = randomPick(rules[level]);
-
-	return {
-		tests:[{a:1,b:2,c:3,actual:rule(1,2,3)}],
-		assumption: '',
-		result: undefined,
-		val:{a:'',b:'',c:''},
-		level,
-	};
-}
-class App extends Component {
-  state:State = getState(0);
-
-	//@ts-ignore
-  addTest = (newTest:Test) => this.setState(state => ({ ...state, tests: [...state.tests, newTest ]}));
-	setAssumption = (assumption:string) => this.setState(state => ({ ...state, assumption}));
-	setResult = (result:boolean) => this.setState(state => ({ ...state, result}));
-
-	restart = () => this.setState(getState(this.state.level));
-
-	test = () => { 
-		const a = +this.state.val.a;
-		const b = +this.state.val.b;
-		const c = +this.state.val.c;
-		this.addTest({a,b,c, actual: rule(a,b,c)});
-	};
-
-	updateVal = (key:'a'|'b'|'c', evt:Event) => {
-		const val:Values = {...this.state.val};
-		val[key] = +(evt.target as HTMLInputElement).value;
-		this.setState({...this.state, val});
-	}
-
-	updateAssumption = (evt:Event) => this.setAssumption((evt.target as HTMLInputElement).value);
-
-	procFormAndCancelSubmit = (method:(evt:Event)=>void,evt:Event) => { 
-		method(evt)
-		
-		evt.preventDefault();
-		return false;
-	}
-
-	makeAssumption = () => { 
-		let assumption = this.state.assumption as string;
-
-		if (!assumption || assumption.length <= 5) {
-			return;
+		constructor(assumption:string) {
+			this.assumption = assumption;
 		}
 
-		console.log(assumption);
-		if (assumption.indexOf('=>') > -1 && !confirm(`'=>' in JS doesn't mean equal-or-great. Do you with to process?`)) {
-			return
+		test(a:number,b:number,c:number) {
+			return eval(this.assumption);
+		}
+	}
+
+	const rules:Rule[][] = [
+		[(a,b,c)=>(a<b && b<c)],
+		[
+			(a,b,c)=>(a>b && b>c),
+			(a,b,c)=>(a+b < c),
+			(a,b,c)=>(a+b > c),
+			(a,b,c)=>(a+c < b),
+			(a,b,c)=>(a+c > b),
+			(a,b,c)=>(b+c < a),
+			(a,b,c)=>(b+c > a),
+			(a,b,c)=>(a+b === a),
+			(a,b,c)=>(a+c === b),
+			(a,b,c)=>(a+b === c)
+		],
+		[
+			(a,b,c)=>(a<=b && b<=c),
+			(a,b,c)=>(a<=b && b<c),
+			(a,b,c)=>(a<b && b<=c),
+			(a,b,c)=>(a>=b && b>=c),
+			(a,b,c)=>(a>b && b>=c),
+			(a,b,c)=>(a>=b && b>c),
+			(a,b,c)=>(a+b <= c),
+			(a,b,c)=>(a+b >= c),
+			(a,b,c)=>(a+c <= b),
+			(a,b,c)=>(a+c >= b),
+			(a,b,c)=>(b+c <= a),
+			(a,b,c)=>(b+c >= a)
+		]];
+
+	let rule:Rule = rules[0][0];
+
+	function randomPick(arr:any[]):any {
+		return arr[Math.floor(Math.random()*arr.length)];
+	}
+
+	function getState(level:number):State {
+		if (level < rules.length - 1) {
+			level++;
+		}
+		
+		rule = randomPick(rules[level]);
+
+		return {
+			tests:[{a:1,b:2,c:3,actual:rule(1,2,3)}],
+			assumption: '',
+			result: undefined,
+			val:{a:'',b:'',c:''},
+			level
+		};
+	}
+	class App extends Component {
+		state:State = getState(0);
+
+		//@ts-ignore
+		addTest = (newTest:Test) => this.setState(state => ({ ...state, tests: [...state.tests, newTest ]}));
+		setAssumption = (assumption:string) => this.setState(state => ({ ...state, assumption}));
+		setResult = (result:boolean) => this.setState(state => ({ ...state, result}));
+
+		restart = () => this.setState(getState(this.state.level));
+
+		test = () => { 
+			const a = +this.state.val.a;
+			const b = +this.state.val.b;
+			const c = +this.state.val.c;
+			this.addTest({a,b,c, actual: rule(a,b,c)});
+		};
+
+		updateVal = (key:'a'|'b'|'c', evt:Event) => {
+			const val:Values = {...this.state.val};
+			val[key] = +(evt.target as HTMLInputElement).value;
+			this.setState({...this.state, val});
 		}
 
-		const hypothesis:HypothesisTest = new HypothesisTest(assumption);
-		
-		for (let a=0;a<10;a++) {
-			for (let b=0;b<10;b++) {
-				for (let c=0;c<10;c++) {
-					try {
-						if (hypothesis.test(a,b,c) !== rule(a,b,c)) {
+		updateAssumption = (evt:Event) => this.setAssumption((evt.target as HTMLInputElement).value);
+
+		procFormAndCancelSubmit = (method:(evt:Event)=>void,evt:Event) => { 
+			method(evt)
+			
+			evt.preventDefault();
+			return false;
+		}
+
+		makeAssumption = () => { 
+			let assumption = this.state.assumption as string;
+
+			if (!assumption || assumption.length <= 5) {
+				return;
+			}
+
+			console.log(assumption);
+			if (assumption.indexOf('=>') > -1 && !confirm(`'=>' in JS doesn't mean equal-or-great. Do you with to process?`)) {
+				return
+			}
+
+			const hypothesis:HypothesisTest = new HypothesisTest(assumption);
+			
+			for (let a=0;a<10;a++) {
+				for (let b=0;b<10;b++) {
+					for (let c=0;c<10;c++) {
+						try {
+							if (hypothesis.test(a,b,c) !== rule(a,b,c)) {
+								return this.setResult(false);
+							}
+						}
+						catch(err) {
+							console.error(err);
 							return this.setResult(false);
 						}
 					}
-					catch(err) {
-						console.error(err);
-						return this.setResult(false);
-					}
 				}
 			}
+			this.setResult(true);
+		};
+
+		render(props:any, state:State) {
+			return (
+				<main>
+					<section class="experiment">
+						<h2>1. Experiment</h2>
+						<form onSubmit={this.procFormAndCancelSubmit.bind(null, this.test)} className="testForm">
+							<input type="number" min={0} max={99} value={state.val.a} onInput={this.updateVal.bind(null,'a')} />
+							<input type="number" min={0} max={99} value={state.val.b} onInput={this.updateVal.bind(null,'b')}/>
+							<input type="number" min={0} max={99} value={state.val.c} onInput={this.updateVal.bind(null,'c')} />
+							<button type="submit">Check</button>
+						</form>
+					</section>
+					<aside class="results">
+						<h2>2. Observe The Results</h2>
+						<ol>{ state.tests.map(({a, b, c, actual}) => <li><span>{a}</span><span>{b}</span><span>{c}</span><span> {actual ? '✅': '❌'}</span></li>)}</ol>
+					</aside>
+					<section class="hyposis">
+						<h2>3. Submit Your Hypothesis</h2>
+						<div class="warning">Careful! you can only submit once!</div>
+						<form onSubmit={this.procFormAndCancelSubmit.bind(null, this.makeAssumption)}>
+							<code><pre>(a,b,c) =&gt; {'{'} return <input value={state.assumption} onInput={this.updateAssumption} type="text" minLength={5}/> {'}'};</pre></code>
+							<button type="submit">Submit</button>
+						</form>
+						{ (state.result!==undefined) ? (state.result!==true ?
+							<dialog open>
+								<h2>Fail!</h2>
+								You suggest <code><pre class="expected">function (a, b, c) {'{'} return ({state.assumption}); {'}'}</pre></code>
+								But the rule was <code><pre class="actual">{rule.toString()}</pre></code>
+								<button onClick={this.restart} class="restart">🔄 Try Another</button>
+							</dialog> : <dialog open>
+								Well done!
+								<button onClick={this.restart} class="restart">🔄 Try Another</button>
+								</dialog>) :'' }
+					</section>
+				</main>
+			);
 		}
-		this.setResult(true);
-	};
+	}
 
-  render(props:any, state:State) {
-    return (
-      <main>
-				<section class="experiment">
-					<h2>1. Experiment</h2>
-					<form onSubmit={this.procFormAndCancelSubmit.bind(null, this.test)} className="testForm">
-						<input type="number" min={0} max={99} value={state.val.a} onInput={this.updateVal.bind(null,'a')} />
-						<input type="number" min={0} max={99} value={state.val.b} onInput={this.updateVal.bind(null,'b')}/>
-						<input type="number" min={0} max={99} value={state.val.c} onInput={this.updateVal.bind(null,'c')} />
-						<button type="submit">Check</button>
-					</form>
-				</section>
-				<aside class="results">
-					<h2>2. Observe The Results</h2>
-					<ol>{ state.tests.map(({a, b, c, actual}) => <li><span>{a}</span><span>{b}</span><span>{c}</span><span> {actual ? '✅': '❌'}</span></li>)}</ol>
-				</aside>
-				<section class="hyposis">
-					<h2>3. Submit Your Hypothesis</h2>
-					<div class="warning">Careful! you can only submit once!</div>
-					<form onSubmit={this.procFormAndCancelSubmit.bind(null, this.makeAssumption)}>
-						<code><pre>(a,b,c) =&gt; {'{'} return <input value={state.assumption} onInput={this.updateAssumption} type="text" minLength={5}/> {'}'};</pre></code>
-						<button type="submit">Submit</button>
-					</form>
-					{ (state.result!==undefined) ? (state.result!==true ?
-						<dialog open>
-							<h2>Fail!</h2>
-							You suggest <code><pre class="expected">function (a, b, c) {'{'} return ({state.assumption}); {'}'}</pre></code>
-							But the rule was <code><pre class="actual">{rule.toString()}</pre></code>
-							<button onClick={this.restart} class="restart">🔄 Try Another</button>
-						</dialog> : <dialog open>
-							Well done!
-							<button onClick={this.restart} class="restart">🔄 Try Another</button>
-							</dialog>) :'' }
-				</section>
-			</main>
-    );
-  }
-}
-
-window.addEventListener('load', () => render(<App />, document.body));
+	window.addEventListener('load', () => render(<App />, document.body));
+})();
